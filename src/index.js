bf668e42018de5c798adda1a86bcaf7c64eb1d04
Redouane Achouri <achouri.a.r@gmail.com>
2020-06-16 21:09:13 +0200

    Added integration for GraphQL client.
    - Imported library and added authentication.
    - Performed basic query with GQL for testing.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 "use strict"
 
-const express = require('express')
-const expressPlayground = require('graphql-playground-middleware-express').default
+const fetch = require('node-fetch');
+const express = require("express");
+const expressPlayground = require("graphql-playground-middleware-express").default;
+const { InMemoryCache } = require("apollo-cache-inmemory");
+const { HttpLink } = require("apollo-link-http");
+const { ApolloClient, gql } = require("apollo-boost");
+
 
 // Load configuration
 const config = require('../config/config')
 const secret = require('../config/secret')
 
 const PORT = config['default']['port'] || 4000
+
+//
+const httpLink = new HttpLink({
+    uri: config['github-api']['url'],
+    fetch: fetch,
+    headers: {
+        'Authorization': `Bearer ${secret['github-api']['token']}`
+    }
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+client
+    .query({
+        query: gql`
+query { 
+  viewer { 
+    login
+  }
+}
+        `
+    })
+    .then(result => console.log(JSON.stringify(result, null, 4)))
+    .catch(error => console.log(JSON.stringify(error, null, 4)));
 
 
 const app = express()
@@ -16,7 +48,7 @@ app.get(
     expressPlayground({
         endpoint: config['github-api']['url'],
         headers: {
-            "Authorization": "Bearer " + secret['github-api']['token']
+            "Authorization": `Bearer ${secret['github-api']['token']}`
         }
     }),
 )
